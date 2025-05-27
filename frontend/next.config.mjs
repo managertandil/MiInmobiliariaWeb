2af6db050cf5fe43496181e64b0301886513ac07
managertandil <manager.tandil@gmail.com>
2025-05-27 17:04:57 -0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   // reactStrictMode: true,
 
   images: {
-    remotePatterns: [
+    // remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
